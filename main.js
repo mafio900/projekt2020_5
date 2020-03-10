9b56a22b4ecc3b61650f9f39f93d970a3573bd95
HighElo <admin@highelo.pl>
2020-03-10 20:12:07 +0100

    Revert "Change application to be a desktop app"
    This reverts commit 2ce266828bf54b6ed9bf66d3aa60d4ae6ea18bc7.

--- main.js
+++ main.js
@@ -1,66 +1,101 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
-
-const {app, BrowserWindow, Menu} = electron;
-
-let mainWindow;
-
-//Listen for app to be ready
-
-app.on('ready', function(){
-    //Create new window
-    mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720
-    });
-    //Load html
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'main.html'),
-        protocol: 'file',
-        slashes: true
-    }));
-    //Quit when closed
-    mainWindow.on('close', function () {
-        app.quit();
-    });
-
-    //Build menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    //Insert menu
-    Menu.setApplicationMenu(mainMenu);
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "dane.txt",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
 });
+var lines = [];
 
-//Create menu template
-const mainMenuTemplate = [
-    {
-        label: 'File',
-        submenu:[
-            {
-                label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click(){
-                    app.quit();
-                }
-            }
-        ]
+
+var klasaA = new Array();
+var klasaB = new Array();
+var klasaC = new Array();
+
+function coordinate(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function processData(allText) {
+
+    var allTextLines = allText.split(/\r\n|\n/);
+
+    for (var i = 0; i < allTextLines.length; i++) {
+        allTextLines[i].split(',');
+        lines.push(allTextLines[i].split(','));
     }
-];
 
-//Add developer tools
-if(process.env.NODE_ENV !== 'production'){
-    mainMenuTemplate.push({
-        label: 'Dev tools',
-        submenu: [
-            {
-                label: 'Toggle dev tools',
-                click(item, focusedWindow){
-                    focusedWindow.toggleDevTools();
-                }
-            },
-            {
-                role: 'reload'
+
+    //przydziel do klasy
+    for (var i = 0; i < lines.length; i++) {
+        if((lines[i][2]) === "KlasaA")
+            klasaA.push(new coordinate( Number.parseInt(lines[i][0]),  Number.parseInt(lines[i][1]) ));
+        if((lines[i][2]) === "KlasaB")
+            klasaB.push(new coordinate( Number.parseInt(lines[i][0]), Number.parseInt(lines[i][1]) ));
+        if((lines[i][2]) === "KlasaC")
+            klasaC.push(new coordinate( Number.parseInt(lines[i][0]), Number.parseInt(lines[i][1]) ));
+        }
+
+
+
+console.log(klasaA);
+console.log(klasaB);
+console.log(klasaC);
+
+
+
+//wykres
+var ctx = document.getElementById('wykres').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Klasa A',
+            data: klasaA,
+            backgroundColor:
+                'rgba(0, 255, 0, 0.9)',
+            borderColor:
+                'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            radius: 10,
+            pointStyle: "triangle"
+        },{
+            label: 'Klasa B',
+            data: klasaB,
+            backgroundColor:
+                'rgba(255, 99, 132, 0.9)',
+            borderColor:
+                'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            radius: 10,
+            pointStyle: "rect"
+        },{
+            label: 'Klasa C',
+            data: klasaC,
+            backgroundColor:
+                'rgba(54, 162, 235, 0.9)',
+            borderColor:
+                'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            radius: 10,
+            pointStyle: "circle"
+        }]
+    },
+    options: {
+        legend: {
+            display: true,
+            labels: {
+                padding:10
             }
-        ]
-    });
+        },
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+});
 }
