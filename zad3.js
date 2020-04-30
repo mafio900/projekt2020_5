eb5d7af178f6c09a85c8490d79955b6fb7abeb67
flasu <022szymon@gmail.com>
2020-04-30 17:07:21 +0200

    Update zad3.js
    Creating an algorithm that checks the accuracy of k-NN method classification

--- zad3.js
+++ zad3.js
@@ -4,6 +4,9 @@ let splitInput = document.getElementById("splitInput");
 let uczacy = [];
 let testujacy = [];
 
+const p = 2;
+const k = 3;
+
 ipcRenderer.on('data', (event, data) => {
     processData(data.msg);
 });
@@ -11,10 +14,12 @@ ipcRenderer.on('data', (event, data) => {
 let names = [];
 let lines = [];
 let data = [];
+let test = [];
 function processData(allText) {
     names = [];
     lines = [];
     data = [];
+    test = [];
     let inputs = document.getElementById("inputs");
     inputs.style.display = "flex";
     let allTextLines = allText.split(/\r\n|\n/);
@@ -36,11 +41,16 @@ function processData(allText) {
     }
     names.forEach((name, i)=>{
         data[i] = [];
+        test[i] = [];
         for(let line of lines){
             let xd = [];
             if(line[line.length-1]===name){
                 line.pop();
                 data[i].push(line);
+                for(let l of line){
+                    xd.push(Number.parseInt(l));
+                }
+                test[i].push(xd);
             }
         }
     });
@@ -81,5 +91,71 @@ splitFile.addEventListener("click", ()=>{
         testujacy = [...testujacy, ...d];
     }
 });
+
+const dataText = document.getElementById("dataText");
+calculate.addEventListener("click", ()=>{
+    for(let [i, po] of testujacy.entries()){
+        const fn = find_neighbors(po);
+        const mv = majority_vote(fn);
+        console.log("Podany punkt o wspolrzednych: "+ po + " nalezy do: "+names[mv]);
+        const node = document.createElement("p");
+        const br = document.createElement("br");
+        node.textContent = (i+1)+". Podany punkt o wspolrzednych: "+ po + " nalezy do: "+names[mv];
+        dataText.appendChild(node);
+        dataText.appendChild(br);
+    }
+});
+
+function distance(p1, p2) {
+    let sum = 0;
+    for(let i =0; i < p1.length-1; i++){
+        sum += Math.pow(Math.abs(p1[i] - p2[i]), p);
+    }
+    sum = Math.pow(sum, 1/p);
+    return sum;
+}
+
+function find_neighbors(point) {
+    const dists = [];
+    for (let i = 0; i < uczacy.length; i++) {
+        for(let j = 0; j < uczacy[i].length; j++) {
+            const dist = distance(point, uczacy[i][j]);
+            dists.push( [ dist, [uczacy[i][j], i] ] );
+        }
+    }
+    dists.sort(function(a, b) { return a[0] - b[0]});
+    const neighbors = [];
+    for (let i = 0; i < k && i < dists.length; i++) {
+        neighbors.push(dists[i][1]);
+    }
+    return neighbors;
+}
+
+function majority_vote(ps) {
+    let votes = new Map();
+    for (let c = 0; c < names.length; c++) {
+        votes.set(names[c], 0);
+    }
+    for (let c = 0; c < names.length; c++) {
+        for (let i = 0; i < ps.length; i++) {
+            if(c===ps[i][1]) {
+                votes.set(names[c], votes.get(names[c]) + 1);
+            }
+        }
+    }
+
+    var max_votes = 0;
+    var winner = null;
+    for (var c = 0; c < names.length; c++) {
+        if (votes.get(names[c]) === max_votes) {
+            winner = null;
+        } else if (votes.get(names[c]) > max_votes) {
+            max_votes = votes.get(names[c]);
+            winner = c;
+        }
+    }
+    return winner;
+}
+
 
 
