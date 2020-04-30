27653390666dc334fab656772de2757aa84dc60a
mafio900 <mafio900@gmail.com>
2020-04-30 17:03:08 +0200

    create two arrays for learning and testing

--- zad3.js
+++ zad3.js
@@ -1,5 +1,8 @@
 const electron = require('electron');
 const {ipcRenderer, remote} = electron;
+let splitInput = document.getElementById("splitInput");
+let uczacy = [];
+let testujacy = [];
 
 ipcRenderer.on('data', (event, data) => {
     processData(data.msg);
@@ -43,5 +46,40 @@ function processData(allText) {
     });
 }
 
+const splitFile = document.getElementById("splitFile");
+const calculate = document.getElementById("calculate");
+splitFile.addEventListener("click", ()=>{
+    let ileW = Number.parseInt(splitInput.value);
+    let toSlice = [];
+    for(let i = 0; i < data.length; i++){
+        toSlice[i] = [];
+        for(let j = 0; j < data[i].length; j++){
+            toSlice[i][j] = data[i][j];
+        }
+    }
+    splitInput.value = "";
+    calculate.style.display = "flex";
+    for(let i = 0; i < toSlice.length; i++) {
+        uczacy[i] = [];
+        if(toSlice[i].length <= ileW/toSlice.length){
+            uczacy[i] = toSlice[i];
+            for (let n = 0; n < (ileW/toSlice.length) - toSlice[i].length; n++) {
+                const r = Math.floor(Math.random()*(toSlice[0].length-1));
+                uczacy[0].push(toSlice[0][r]);
+                toSlice[0].splice(r,1);
+            }
+        }
+        else{
+            for (let j = 0; j < ileW / toSlice.length; j++) {
+                const r = Math.floor(Math.random()*(toSlice[i].length-1));
+                uczacy[i].push(toSlice[i][r]);
+                toSlice[i].splice(r,1);
+            }
+        }
+    }
+    for(let d of toSlice){
+        testujacy = [...testujacy, ...d];
+    }
+});
 
 
